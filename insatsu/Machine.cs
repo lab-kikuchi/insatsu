be2bdaafbf838dc246e9d2e26d77ddd919df2244
cheichey <cheichey.1223@gmail.com>
2023-12-04 15:03:05 +0900

    PrintとMachineクラスを追加した

---
+++ insatsu/Machine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace insatsu
+{
+    using Prints = List<Print | null>;
+
+    enum Print_Type
+    {
+        A,B
+    }
+
+    internal class Machine
+    {
+        public List<Prints> schedule;
+        public Print_Type type;
+        public int size;
+        public string name;
+
+        public Machine(Print_Type type, int size, string name)
+        {
+            this.type = type;
+            this.size = size;
+            this.name = name;
+        }
+
+        public void Set_Plan(List<Print> print)
+        {
+            if (check_index()) this.schedule.Add(print);
+        }
+
+        private bool check_index()
+        {
+            if (size == this.schedule.Count) return false;
+            return true;
+        }
+    }
+}
---
+++ insatsu/Print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insatsu
+{
+    internal class Print
+    {
+        public string name;
+        public int circulation;
+        public Print(string name, int circulation)
+        {
+            this.name = name;
+            this.circulation = circulation;
+        }
+    }
+}
