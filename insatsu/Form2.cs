7d55b8a555eeb5434700d900e5091c77ed19c954
cheichey <cheichey.1223@gmail.com>
2024-01-20 14:58:28 +0900

    複数行表示されない問題を修正

--- insatsu/Form2.cs
+++ insatsu/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 using static insatsu.Form1;
 
 namespace insatsu
@@ -25,13 +26,28 @@ namespace insatsu
             this.minimumUnitMinutes = minimumUnitMinutes;
             this.machines = machines;
         }
+
+        private int Get_Max_Count(List<List<Print2>> schedule)
+        {
+            int max = 0;
+
+            for(int i = 0; i < schedule.Count; i++)
+            {
+                if(max < schedule[i].Count)
+                {
+                    max = schedule[i].Count;
+                }
+            }
+
+            return max;
+        }
         private void Form2_Load(object sender, EventArgs e)
         {
             // カラム数を指定
-            dataGridView1.ColumnCount = endTime - beginTime;
+            dataGridView1.ColumnCount = endTime - beginTime + 1;
 
             // 行ヘッダーの作成
-            for (int i = 0; i < endTime - beginTime; i++)
+            for (int i = 0; i < endTime - beginTime + 1; i++)
             {
                 dataGridView1.Columns[i].HeaderText = i + beginTime + "時";
             }
@@ -52,16 +68,30 @@ namespace insatsu
             for (int i = 0; i < machines.Count; i++)
             {
                 var machine = machines[i];
-                for (int j = 0; j < machine.schedule[0].Count; j++)
+                for (int j = 0; j < machine.schedule.Count; j++)
+                {
+                    Console.WriteLine(machine.schedule[j].Count);
+                }
+
+                int max = Get_Max_Count(machine.schedule);
+                for (int j = 0; j < max; j++)
                 {
                     var index = dataGridView1.Rows.Add();
                     dataGridView1.Rows[index].HeaderCell.Value = machine.name;
+
 
                     for(int k = 0; k < machine.schedule.Count; k++)
                     {
 
                         if (machine.schedule[k].Count > j)
                         {
+                            var a = machine.schedule[k];
+                            var b = machine.schedule[k][j].name;
+
+                            Console.WriteLine($"i:{i} j:{j} k:{k}");
+
+                            var c = dataGridView1.Rows[index];
+                            var d = dataGridView1.Rows[index].Cells[k];
                             dataGridView1.Rows[index].Cells[k].Value = machine.schedule[k][j].name;
 
                         }
